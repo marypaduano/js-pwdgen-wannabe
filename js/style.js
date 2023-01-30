8590470aaa059d666fb2619f213a14ff0c5a0070
marypaduano <maria.paduano@gmail.com>
2023-01-30 17:24:46 +0100

    add prompt js

---
+++ js/style.js
@@ -0,0 +1,14 @@
+
+//chiedi all'utente il suo nome
+let firstName = prompt('Ciao. Inserisci qui il tuo Nome')
+//chiedi il suo cognome
+let lastName = prompt ('ora inserisci il tuo Cognome')
+//chiedi il suo colore preferito
+let favColor = prompt ('Qual è il tuo colore preferito?')
+//chiedi un numero
+let reqNum = prompt ('Ultimo sforzo..scrivi un numero')
+reqNum = parseInt(reqNum)
+//scrivi sulla pagina il risultato della concatenazione
+
+const passwordResult = document.getElementById('result')
+passwordResult.innerHTML = ''
